654f81a79456b337249c40763b43f8b380cc49d7
Mekarosi <mekarosi_assa@yahoo.com>
2022-04-05 05:57:50 -0700

    Use fetched data to deplay cryptocurrency details

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY
     'X-RapidAPI-Key': '7ed2ce3f10mshc46fb3c2f56b5bap1145a7jsn4f5a2d713bc5'
 }
 
@@ -19,11 +18,14 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
-    })
+    }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        })
 })
 
 })
 
 export const {
-    useGetCryptosQuery,
+    useGetCryptosQuery, useGetCryptoDetailsQuery
 } = cryptoApi
