a4193bc0fb110a1bddb1179e3e9ad4af479002fa
Mekarosi <mekarosi_assa@yahoo.com>
2022-04-12 04:47:59 -0700

    Remove console log

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -23,8 +23,6 @@ const CryptoDetails = () => {
   if(isFetching) return <Loader />
 
 
-  // console.log(data?.data?.coin)
-  console.log(coinHistory)
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
